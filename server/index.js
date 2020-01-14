0806200f1b9c2edce57413e0b768eda4fffa0128
Tausif Solaiman <tausifsolaiman@gmail.com>
2020-01-14 16:23:13 -0500

    setup express & testing APIs

--- server/index.js
+++ server/index.js
@@ -1,4 +1,28 @@
-var Client = require('coinbase').Client;
-var client = new Client({'accessToken': accessToken, 'refreshToken': refreshToken});
+const express = require('express')
+const PORT = 19006
+const app = express()
+
+// const RH = require('robinhood-node');
+// const rh = new RH({ user: 'RH user', password: 'RH password' });
+const RobinHood = require('robinhood-api');
+const robinhood = new RobinHood();
+console.log('Listcalls', robinhood.listCalls());
+
+
+  // const account = new rh.Account();
+ // console.log(await account.user);
+
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+  
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error')
+  })
+
+  // start listening (and create a 'server' object representing our server)
+   app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  )
 
 
